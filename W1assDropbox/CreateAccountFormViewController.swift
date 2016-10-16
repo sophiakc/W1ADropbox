4b9eafb0354b4960816b76f809287f728a776559
sophiakc <sophiakc@gmail.com>
2016-10-16 15:25:09 -0700

    added custom class View Controller for CreateAccountForm and SignInAccountForm

---
+++ W1assDropbox/CreateAccountFormViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CreateAccountFormViewController.swift
+//  W1assDropbox
+//
+//  Created by Sophia KC on 16/10/16.
+//  Copyright © 2016 Sophia KC. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+}
---
+++ W1assDropbox/SignInFormViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignInFormViewController.swift
+//  W1assDropbox
+//
+//  Created by Sophia KC on 16/10/16.
+//  Copyright © 2016 Sophia KC. All rights reserved.
+//
+
+import UIKit
+
+class SignInFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+}
