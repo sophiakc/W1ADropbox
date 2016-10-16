b9ec1d44441349cb35ed0932e1d7f790c2e67381
sophiakc <sophiakc@gmail.com>
2016-10-16 16:13:04 -0700

    issue 1 solved: SignInForm was not assigned to the appropriate View Controller (custom class) changed it to SignInFormViewController

--- W1assDropbox/SignInAccountRootViewController.swift
+++ W1assDropbox/SignInAccountRootViewController.swift
@@ -18,3 +18,31 @@ class SignInAccountRootViewController: UIViewController {
 
     
 }
+
+
+
+
+
+//
+//  CreateAccountFormViewController.swift
+//  W1assDropbox
+//
+//  Created by Sophia KC on 16/10/16.
+//  Copyright © 2016 Sophia KC. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountFormViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func onTapWelcome(_ sender: AnyObject) {
+        navigationController!.popViewController(animated: true)
+    }
+    
+}
